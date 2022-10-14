a3d233acdd7bc68a757f50d38c6005238c8ec04e
KangYeBin <yb725v@naver.com>
2022-10-14 16:20:01 +0900

    26. 구글 로그인 연동 구현하기 - 1

--- app/src/main/java/com/yb/part6_chapter01/screen/main/my/MyFragment.kt
+++ app/src/main/java/com/yb/part6_chapter01/screen/main/my/MyFragment.kt
@@ -1,5 +1,12 @@
 package com.yb.part6_chapter01.screen.main.my
 
+import android.app.Activity
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.yb.part6_chapter01.R
 import com.yb.part6_chapter01.databinding.FragmentMyBinding
 import com.yb.part6_chapter01.screen.base.BaseFragment
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -8,6 +15,42 @@ class MyFragment : BaseFragment<MyViewModel, FragmentMyBinding>() {
     override val viewModel by viewModel<MyViewModel>()
 
     override fun getViewBinding(): FragmentMyBinding = FragmentMyBinding.inflate(layoutInflater)
+
+    private val gso: GoogleSignInOptions by lazy {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+    }
+
+    private val gsc by lazy { GoogleSignIn.getClient(requireActivity(), gso) }
+
+    private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+    private val loginLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                try {
+                    task.getResult(ApiException::class.java)?.let { account ->
+                        viewModel.saveToken(account.idToken ?: throw Exception())
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+    override fun initViews() = with(binding) {
+        loginButton.setOnClickListener {
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = gsc.signInIntent
+        loginLauncher.launch(signInIntent)
+    }
 
     override fun observeData() {}
 
--- app/src/main/java/com/yb/part6_chapter01/screen/main/my/MyViewModel.kt
+++ app/src/main/java/com/yb/part6_chapter01/screen/main/my/MyViewModel.kt
@@ -3,4 +3,8 @@ package com.yb.part6_chapter01.screen.main.my
 import com.yb.part6_chapter01.screen.base.BaseViewModel
 
 class MyViewModel: BaseViewModel() {
+
+    fun saveToken(idToken: String) {
+        TODO("Not yet implemented")
+    }
 }
