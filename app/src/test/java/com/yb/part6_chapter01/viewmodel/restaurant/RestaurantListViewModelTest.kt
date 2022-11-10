ae2663a0d970b34724b3b2ae384d9e47dbe7bc6b
KangYeBin <yb725v@naver.com>
2022-11-10 15:36:23 +0900

    37. 테스트 코드 세팅

---
+++ app/src/test/java/com/yb/part6_chapter01/viewmodel/restaurant/RestaurantListViewModelTest.kt
@@ -0,0 +1,9 @@
+package com.yb.part6_chapter01.viewmodel.restaurant
+
+import com.yb.part6_chapter01.screen.main.home.restaurant.RestaurantCategory
+import com.yb.part6_chapter01.viewmodel.ViewModelTest
+
+internal class RestaurantListViewModelTest : ViewModelTest() {
+
+    private val restaurantCategory = RestaurantCategory.ALL
+}
